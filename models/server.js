d743d77d59f43bb0c8a528d919eca283ad1ac496
Gustavo Angel <gustavo.18361039@gmail.com>
2022-04-06 17:27:24 -0500

    Auth route

--- models/server.js
+++ models/server.js
@@ -7,7 +7,7 @@ class Server{
         this.app = express(); 
         this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios';
-        //this.authPath = '/api/auth';
+        this.authPath = '/api/auth';
 
         //Connection to DB
        this.connection();
@@ -18,7 +18,7 @@ class Server{
     }
 
     routes(){
-        //this.app.use(this.authPath, require('../routes/auth'));
+        this.app.use(this.authPath, require('../routes/auth'));
         this.app.use(this.usuariosPath, require('../routes/usuarios'));
     }
 
