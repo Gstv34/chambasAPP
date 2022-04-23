e106c4f85d9eda54091bf2e47b2142b286971500
Gustavo Angel <gustavo.18361039@gmail.com>
2022-04-22 23:08:00 -0500

    Freelancer middlewares & queries

--- controllers/freelancer.js
+++ controllers/freelancer.js
@@ -1,10 +1,14 @@
 const { request, response } = require("express");
+const { default: mongoose } = require("mongoose");
 const Free = require('../models/freelancer');
 
-const freeGet = async(req, res = response) =>{
-    const {page, limit = 5, from = 0, stars = "5"} = req.query; 
+const freePopularGet = async(req, res = response) =>{
+    const {page, limit = 5, from = 0, stars = "5", category = null} = req.query; 
 
-    const query = {rank : stars};
+    let query = {rank : stars};
+    if(category!= null){
+        query.categories = category;
+    }
 
     const [total, usuarios] = await Promise.all([
         Free.countDocuments(query),
@@ -14,7 +18,30 @@ const freeGet = async(req, res = response) =>{
     ]);
 
     res.json({
-        route : `get: {{url}}/api/usuarios/? limit = ${limit} && from = ${from} && stars = ${stars}`, 
+        route : `get: {{url}}/api/usuarios/free? limit = ${limit} && from = ${from} && stars = ${stars}`, 
+        total,
+        usuarios
+    });
+
+}
+const freeCatGet = async(req, res = response) =>{
+    const {page, limit = 5, from = 0} = req.query; 
+
+    const {id} = req.params;
+
+    const _id = mongoose.Types.ObjectId(id);
+
+    const query = {categories : _id};
+
+    const [total, usuarios] = await Promise.all([
+        Free.countDocuments(query),
+        Free.find(query).skip(Number(from)).limit(Number(limit))
+        .populate('categories', {usuario:0})
+        .populate('usuario', {  _id: 1, name: 1, lastname: 1})
+    ]);
+
+    res.json({
+        route : `get: {{url}}/api/usuarios/free? limit = ${limit} && from = ${from} && category = ${id}`, 
         total,
         usuarios
     });
@@ -35,6 +62,7 @@ const {exp, skills, social, categories, rank, usuario} = req.body;
 }
 
 module.exports = {
-    freeGet,
-    freePost
+    freePopularGet,
+    freePost,
+    freeCatGet
 }
--- routes/freelancer.js
+++ routes/freelancer.js
@@ -1,11 +1,21 @@
 const {Router} = require('express');
 const { check } = require('express-validator');
 
-const {freePost, freeGet} = require('../controllers/freelancer');
-const {validarCampos} = require('../middlewares')
+const {freePost, freePopularGet,freeCatGet} = require('../controllers/freelancer');
+const {validarCampos, validarJWT} = require('../middlewares')
 
 const router =  Router();
-router.get('/',freeGet);
-router.post('/',freePost);
+router.get('/',freePopularGet);
+router.post('/',
+[   validarJWT,
+    check('usuario','No es un usuario valido').isMongoId(),
+    validarCampos
+],
+freePost);
+router.get('/category/:id',
+[   validarJWT,
+    check('id','No es un usuario valido').isMongoId(),
+    validarCampos
+],freeCatGet);
 
 module.exports = router;
