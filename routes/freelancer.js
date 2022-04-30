9578b2525d8754911b39b78387b61577af94d4b4
Gustavo Angel <gustavo.18361039@gmail.com>
2022-04-30 12:00:59 -0500

    ValidarJWT remove

--- routes/freelancer.js
+++ routes/freelancer.js
@@ -8,10 +8,6 @@ const router =  Router();
 router.get('/',freePopularGet);
 router.get('/:id',
     [
-<<<<<<< HEAD
-=======
-        validarJWT,
->>>>>>> e0c30cfef2bf410d5c3113af99145af69f098fd1
         check('id','No es un usuario valido').isMongoId(),
         validarCampos
     ],
